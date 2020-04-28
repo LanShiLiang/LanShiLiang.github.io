9cd8c8769b74174c2311b289437f894e43e2e46c
giveMeBug <389443599@qq.com>
2020-04-28 17:54:18 +0800

    update

--- miao/lodash/LanShiLiang-lodash.js
+++ miao/lodash/LanShiLiang-lodash.js
@@ -1,4 +1,10 @@
 var lanshiliang = {
+  /**
+   *  * 用户名不要以数字开头，以数字开头的同学请至github更改用户名，不用重新注册
+      * 文件名中最好不要包含中划线，如果包含中划线，请将中划线换为下划线然后做为变量名及文件名的前缀
+      * 文件名(<username>-lodash.js)全小写，用户名与'lodash'之间的分隔符为中划线
+      * 文件中的变量名全小写: var foobar = {chunk: function(){}}
+   */
   isNull: function (val) {
     if (val === null) {
       return true
