ef2526dbf6e703da0c64fa9a4b9500008e70267b
giveMeBug <389443599@qq.com>
2020-04-28 17:45:42 +0800

    update

---
+++ miao/lodash/LanShiLiang-lodash.js
@@ -0,0 +1,31 @@
+var lanshiliang = {
+  isNull: function (val) {
+    if (val === null) {
+      return true
+    } else {
+      return false
+    }
+  },
+  isNaN: function (val) {
+    if (val !== val) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
