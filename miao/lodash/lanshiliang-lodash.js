fc31dc343d005d128262b1a1b26cf57284ba97b4
giveMeBug <389443599@qq.com>
2020-04-30 20:49:27 +0800

    4.30

--- miao/lodash/lanshiliang-lodash.js
+++ miao/lodash/lanshiliang-lodash.js
@@ -5,6 +5,28 @@ var lanshiliang = {
       * 文件名(<username>-lodash.js)全小写，用户名与'lodash'之间的分隔符为中划线
       * 文件中的变量名全小写: var foobar = {chunk: function(){}}
    */
+
+  /**
+   chunk按size分割数组 用slice切割
+   */
+  chunk: function (arrary, size) {
+    size = parseInt(size)
+    let length = arrary == null ? 0 : arrary.length
+    if (!length || size < 1) {
+      return []
+    }
+    const result = new Array(Math.ceil(length / size))
+    let index = 0
+    let resindex = 0
+    while (index < length) {
+      result[resindex++] = arrary.slice(index, index += size)
+    }
+    return result
+  },
+
+
+
+
   isNull: function (val) {
     if (val === null) {
       return true
